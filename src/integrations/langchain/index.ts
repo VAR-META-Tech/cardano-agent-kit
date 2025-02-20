865bfcf1ee059e6c15168236b4c7fcdc4cf87993
thanhngoc541 <ngochc1@gmail.com>
2025-02-20 12:21:03 +0700

    Update export

--- src/integrations/langchain/index.ts
+++ src/integrations/langchain/index.ts
@@ -5,5 +5,5 @@ import { CardanoToolKit } from "../../tools";
 /**
  * Create AI-powered blockchain tools for a custom toolkit instance.
  */
-export const createLangChainCardanoTools = (toolkit: CardanoToolKit) =>
+export const createLangchainCardanoTools = (toolkit: CardanoToolKit) =>
     createLangchainTools(toolkit, Object.values(actions));
