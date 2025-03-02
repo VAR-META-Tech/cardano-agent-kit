be5fea274261c9d06dd04df6afc82ecd7de0f8fb
thanhngoc541 <ngochc1@gmail.com>
2025-03-02 19:11:13 +0700

    Update

--- src/integrations/eliza/actions/sendLovelace.ts
+++ src/integrations/eliza/actions/sendLovelace.ts
@@ -7,7 +7,7 @@ import {
     type State,
     type Action,
 } from "@elizaos/core";
-import { extractLovelaceTransaction, initializeCardanoToolKit } from "../utils/index";
+import { extractLovelaceTransaction, initializeCardanoToolKit } from "../utils/index.ts";
 
 export const sendLovelaceAction: Action = {
     name: "SEND_LOVELACE",
